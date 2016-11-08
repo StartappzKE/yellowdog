ba1d6eaec3b0c75e2209ae85b49b7c8426f55f35
jijothic <geogtz@yahoo.com>
2016-11-08 20:17:09 +0300

    Added Login

--- app/src/main/java/com/masomohub/Login.java
+++ app/src/main/java/com/masomohub/Login.java
@@ -4,11 +4,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,6 +13,7 @@ import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.answers.LoginEvent;
 import com.digits.sdk.android.AuthCallback;
 import com.digits.sdk.android.Digits;
 import com.digits.sdk.android.DigitsAuthButton;
@@ -23,6 +21,7 @@ import com.digits.sdk.android.DigitsException;
 import com.digits.sdk.android.DigitsSession;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
+
 import io.fabric.sdk.android.Fabric;
 
 public class Login extends AppCompatActivity {
@@ -58,11 +57,15 @@ public class Login extends AppCompatActivity {
         Fabric.with(this, new TwitterCore(authConfig), digitsBuilder.build(), new Answers(), new Crashlytics());
 
         DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
-        digitsButton.setAuthTheme(android.R.style.Theme_Material);
+        digitsButton.setAuthTheme(R.style.AppTheme);
         digitsButton.setCallback(new AuthCallback() {
             @Override
             public void success(DigitsSession session, String phoneNumber) {
                 // TODO: associate the session userID with your user model
+
+                //SessionRecorder.recordSessionActive("Login: digits account active", session);
+                //Answers.getInstance().logLogin(new LoginEvent().putMethod("Digits").putSuccess(true));
+                startActivity(new Intent(Login.this, MainActivity.class));
                 Toast.makeText(getApplicationContext(), "Authentication successful for "
                         + phoneNumber, Toast.LENGTH_LONG).show();
 
@@ -71,7 +74,11 @@ public class Login extends AppCompatActivity {
 
             @Override
             public void failure(DigitsException exception) {
-                Log.d("Digits", "Sign in with Digits failure", exception);
+                Answers.getInstance().logLogin(new LoginEvent().putMethod("Twitter").putSuccess(false));
+                Toast.makeText(getApplicationContext(),
+                        getResources().getString(R.string.toast_twitter_signin_fail),
+                        Toast.LENGTH_SHORT).show();
+                Crashlytics.logException(exception);
             }
         });
 
